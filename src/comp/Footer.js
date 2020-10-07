2078d583dc3a0dcd86634f70de6fede07a12f482
Ethan Ferrier <Fethanerrier@gmail.com>
2020-10-06 19:47:39 -0700

    form started

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -21,13 +21,13 @@ function Footer() {
                     <li className="list-none blod text-gray-800 border-b-4 border-blue-200">Find us on socail media </li>
                     <li className="list-none pt-2 text-sm">
                         <span> <FontAwesomeIcon icon={faFacebook} size='1x' /> </span>
-                        <a href="https://facebook.com" className="hover:text-blue-300 italic" target="_blank">
+                        <a href="https://facebook.com" className="hover:text-blue-200 italic" target="_blank">
                             Facebook
                         </a>
                     </li>
                     <li className="list-none pt-2 text-sm">
                         <span> <FontAwesomeIcon icon={faInstagram} size='1x' /> </span>
-                        <a href="https://instagram.com" className="hover:text-blue-300 italic" target="_blank">
+                        <a href="https://instagram.com" className="hover:text-blue-200 italic" target="_blank">
                             Instagram
                         </a>
                     </li>
@@ -35,7 +35,7 @@ function Footer() {
                         <span>
                             <FontAwesomeIcon icon={faTwitter} size='1x' />
                         </span>
-                        <a href="https://twitter.com" className="hover:text-blue-300 italic" target="_blank">
+                        <a href="https://twitter.com" className="hover:text-blue-200 italic" target="_blank">
                             Twitter
                         </a>
                     </li>
@@ -43,19 +43,19 @@ function Footer() {
                 <ul className="md:flex-1 mx-auto text-center px-4 pt-8 h-auto block">
                     <li className="list-none blod text-gray-800 border-b-4 border-green-200"> More links </li>
                     <li className="list-none pt-2 text-sm ">
-                        <a href="https://www.nickkerley.com" className="hover:text-blue-300 italic" target="_blank">
+                        <a href="https://www.nickkerley.com" className="hover:text-green-200 italic" target="_blank">
                             Nick Kerley Real Estate
                         </a>
                     </li>
                     <li className="list-none pt-2 text-sm">
                         Built and Designed By: &nbsp;
-                        <a href="https://www.ethanferrier.com" className="hover:text-blue-300 italic" target="_blank">
+                        <a href="https://www.ethanferrier.com" className="hover:text-green-200 italic" target="_blank">
                             Ethan Ferrier
                         </a>
                     </li>
                     <li className="list-none pt-2 text-sm">
                         Built in: &nbsp;
-                        <a href="https://reactjs.org" className="hover:text-blue-300 italic" target="_blank">
+                        <a href="https://reactjs.org" className="hover:text-green-200 italic" target="_blank">
                             ReactJs
                         </a>
                     </li>
--- src/comp/Quote.js
+++ src/comp/Quote.js
@@ -4,13 +4,43 @@ import React from 'react';
 
 const Quote = ({ toggelDisplay }) => {
     return (
-        <button onClick={() => toggelDisplay()} className="bold text-lg fixed w-full bg-gray-200 h-full inset-0 z-20 cursor-default opacity-50">
-            <div className="bg-black w-auto text-white opacity-100 block">
-                well hello there this section still needs some work. <br />
-                please come back later to submit your request for a Quote. <br />
-                Thank you
+        <div className="fixed inset-0 w-full h-full z-10">
+            <button onClick={() => toggelDisplay()} className="bold text-lg w-full h-screen bg-gray-200 absolute cursor-default opacity-50 inset-0">
+            </button>
+            <div className="w-2/3 h-auto mx-auto my-20 text-white relative rounded-lg">
+                <div className="w-full bg-white shadow-md rounded px-8 pt-2 pb-8 mb-4">
+                    <div className="flex justify-end">
+                        <button type="button" onClick={() => toggelDisplay()} className="text-gray-400 hover:text-gray-500 hover:bg-gray-100 " aria-label="Close menu">
+                            <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <form className="">
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                Your Email
+                             </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Your Email" />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="intro">
+                                Intro
+                             </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="intro" type="text" placeholder="ie: full kitchen remodel" />
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                                Sign In
+                            </button>
+                            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                                Forgot Password?
+                            </a>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </button>
+        </div>
     )
 }
 

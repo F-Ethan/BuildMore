40fed323fed1c90ac2eac1ec1e3a07ce4246d647
Ethan Ferrier <Fethanerrier@gmail.com>
2020-10-16 19:43:19 -0700

    netlify form support

--- src/comp/Quote.js
+++ src/comp/Quote.js
@@ -7,7 +7,7 @@ const Quote = ({ toggelDisplay }) => {
         <div className="fixed inset-0 w-full h-screen z-10 p-8">
             <button onClick={() => toggelDisplay()} className="bold text-lg w-full h-screen bg-gray-200 absolute cursor-default opacity-50 inset-0">
             </button>
-            <div className="w-2/3 h-full mx-auto my-2 text-white relative rounded-lg bg-white shadow-md overflow-y-auto">
+            <div className="w-full md:w-2/3 h-full mx-auto my-2 text-white relative rounded-lg bg-white shadow-md overflow-y-auto">
                 <div className="flex justify-end p-2 pb-0">
                     <button type="button" onClick={() => toggelDisplay()} className="text-gray-400 hover:text-gray-500 hover:bg-gray-100 " aria-label="Close menu">
                         <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -16,8 +16,9 @@ const Quote = ({ toggelDisplay }) => {
                     </button>
                 </div>
                 <div className="w-full px-8 pt-2 pb-8 mb-4">
-                    <h2 className="block text-gray-500 text-md text-center mb-4 border-b-4 font-bold tracking-wider mb-2">Request a Bid</h2>
-                    <form className="">
+                    <h2 className="block text-gray-500 text-md text-center my-2 border-b-4 font-bold tracking-wider">Request a Bid</h2>
+                    <form name="Quote" method="post">
+                        <input type="hidden" name="form-name" value="Quote" />
                         <div className="mb-4 flex ">
                             <div className="pr-2 flex-1">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
@@ -55,9 +56,9 @@ const Quote = ({ toggelDisplay }) => {
                                 </label>
                                 <div className="relative">
                                     <select className="block appearance-none w-full border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="work-type">
-                                        <option>Remodel</option>
-                                        <option>New Build</option>
-                                        <option>Addition</option>
+                                        <option value="Remodel">Remodel</option>
+                                        <option value="Addition">Addition</option>
+                                        <option value="New Build">New Build</option>
                                     </select>
                                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -66,15 +67,15 @@ const Quote = ({ toggelDisplay }) => {
                             </div>
                             <div className="w-full mb-4 pl-2">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" for="work-type">
-                                    Buget
+                                    budget
                                 </label>
                                 <div className="relative">
                                     <select className="block appearance-none w-full border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="buget">
-                                        <option>$1,000 - $5,000</option>
-                                        <option>$5,000 - $10,000</option>
-                                        <option selected>$10,000 - $50,000</option>
-                                        <option>$50,000 - $100,000</option>
-                                        <option>$100,000 Up</option>
+                                        <option value="1k-5k">$1,000 - $5,000</option>
+                                        <option value="5k-10k">$5,000 - $10,000</option>
+                                        <option value="10k-50k" selected>$10,000 - $50,000</option>
+                                        <option value="50k-100k">$50,000 - $100,000</option>
+                                        <option value="100K+">$100,000 Up</option>
                                     </select>
                                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
